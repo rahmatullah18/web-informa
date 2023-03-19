bf627b6257ae64189a1937b2eb24d358493626e6
unknown <rahmatullahsmakenza@gmail.com>
2023-03-19 22:49:45 +0800

    update ui

---
+++ src/components/content/home/homeFilterProduct.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomeFilterProduct = () => {
+  return (
+    <div className="px-4 py-2 font-bold text-white rounded-lg bg-secondary-200">
+      filter
+    </div>
+  );
+};
--- src/components/content/home/homeProducts.tsx
+++ src/components/content/home/homeProducts.tsx
@@ -2,28 +2,29 @@ import { Heading } from "../../ui/heading/heading";
 import { HomeCard } from "./homeCard";
 import { dataProducts } from "../../../data/dataProducts";
 import { Link } from "react-router-dom";
+import { HomeFilterProduct } from "./homeFilterProduct";
 
 export const HomeProducts = () => {
+  const mapProducts = dataProducts.map((product) => {
+    return (
+      <Link key={product.id} to={`products/${product.slug}`}>
+        <HomeCard
+          urlImge={product.urlImage}
+          color={product.product_color}
+          name={product.product_name}
+        />
+      </Link>
+    );
+  });
+
   return (
     <div className="px-2 space-y-4">
       <div className="flex items-center justify-between">
         <Heading size="lg">Products</Heading>
-        <div className="px-4 py-2 font-bold text-white rounded-lg bg-primary-100">
-          filter
-        </div>
+        <HomeFilterProduct />
       </div>
       <div className="grid w-full grid-cols-2 gap-4 place-items-center">
-        {dataProducts.map((product) => {
-          return (
-            <Link key={product.id} to={`products/${product.slug}`}>
-              <HomeCard
-                urlImge={product.urlImage}
-                color={product.product_color}
-                name={product.product_name}
-              />
-            </Link>
-          );
-        })}
+        {mapProducts}
       </div>
     </div>
   );
