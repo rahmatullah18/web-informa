64b24257d86141c46e6d8bf75fd391379796a888
rahmatullah18 <rahmatullahsmakenza@gmail.com>
2023-03-21 00:58:56 +0800

    menambahkan keranjang

--- src/components/layouts/navbar/navbar.tsx
+++ src/components/layouts/navbar/navbar.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { HiShoppingCart } from "react-icons/hi";
+import { useCart } from "react-use-cart";
 
 export const Navbar = () => {
+  const { cartTotal } = useCart();
+
   return (
     <nav className="flex items-center justify-between h-12 px-2">
       <div className="flex gap-4">
@@ -8,10 +12,15 @@ export const Navbar = () => {
           Home
         </Link>
         <Link className="font-bold text-tertiary-100" to={"/questions"}>
-          Pertanyaan Umum
+          Informasi
         </Link>
       </div>
-      <div className="w-10 h-10 bg-primary-100 rounded-xl"></div>
+      <div className="relative flex flex-col items-center justify-center p-1 rounded-xl">
+        <HiShoppingCart className="w-10 h-10 text-tertiary-100" />
+        <span className="absolute top-0 right-0 flex flex-col items-center justify-center w-5 h-5 font-bold rounded-full shadow-md bg-primary-200">
+          {cartTotal}
+        </span>
+      </div>
     </nav>
   );
 };
